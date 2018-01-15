0ed3cb307daf28711be5187367afceb61f8ee641
Adrian Thompson <31362409+athompson-tamu@users.noreply.github.com>
2018-01-14 18:01:20 -0600

    changed to generic ROOT file path

--- test/muon_analyzer_cfg.py
+++ test/muon_analyzer_cfg.py
@@ -9,7 +9,7 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 process.source = cms.Source("PoolSource",
 # replace 'myfile.root' with the source file you want to use
 fileNames = cms.untracked.vstring(
-'file:/afs/cern.ch/work/a/adthomps/public/9_4_0_alignment/CMSSW_9_4_0_pre2/src/analysis/642B085F-1E8E-E711-9B76-02163E01A20D.root'
+'root://eoscms.cern.ch://store/data/Run2017C/SingleMuon/ALCARECO/MuAlCalIsolatedMu-PromptReco-v3/000/301/960/00000/642B085F-1E8E-E711-9B76-02163E01A20D.root'
 )
 )
 
